b7b3bc85118130d01d331b0de03d5090c2c93872
Medhat Ramadan <136108426+MedhatRamdan@users.noreply.github.com>
2024-11-09 14:00:50 +0200

    Update Gate.java
    adding displayTotalServedCars() function

--- src/main/java/com/parking/simulation/Gate.java
+++ src/main/java/com/parking/simulation/Gate.java
@@ -4,15 +4,18 @@ import java.util.List;
 //Represents each gate as a separate thread to handle car arrivals.
 class Gate implements Runnable {
     private final int gateId;
+    private int totalServedCars;
     private final List<Car> carQueue;
 
     public Gate(int gateId) {
         this.gateId = gateId;
         this.carQueue = new ArrayList<>();
+        this.totalServedCars = 0;
     }
 
     public void addCar(Car car) {
         carQueue.add(car);
+        totalServedCars++;
     }
 
     @Override
@@ -20,5 +23,9 @@ class Gate implements Runnable {
         for (Car car : carQueue) {
             new Thread(car).start();
         }
+    }
+
+    public void displayTotalServedCars() {
+        System.out.println("Gate " + gateId + " served " + totalServedCars + " cars.");
     }
 }
