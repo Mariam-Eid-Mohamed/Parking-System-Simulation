c7a872098069da61c471da961495eeb336683699
Medhat Ramadan <136108426+MedhatRamdan@users.noreply.github.com>
2024-11-12 00:15:11 +0200

    Update Gate.java

--- src/main/java/com/parking/simulation/Gate.java
+++ src/main/java/com/parking/simulation/Gate.java
@@ -2,7 +2,7 @@ package com.parking.simulation;
 import java.util.ArrayList;
 import java.util.List;
 //Represents each gate as a separate thread to handle car arrivals.
-class Gate implements Runnable {
+class Gate {
     private final int gateId;
     private int counter = 0; // counting cars that pass across each gate 
     private final List<Car> carQueue;
@@ -17,12 +17,12 @@ class Gate implements Runnable {
         counter++;
     }
 
-    @Override
-    public void run() {
-        for (Car car : carQueue) {
-            new Thread(car).start();
-        }
-    }
+    // @Override
+    // public void run() {
+    //     for (Car car : carQueue) {
+    //         new Thread(car).start();
+    //     }
+    // }
 
     public List<Car> getCarQueue() {
         return carQueue;
